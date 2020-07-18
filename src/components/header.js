35128ad0cfb17c376ec03436d2e7c3bfb6d54d28
JSGehlen <jeremygehlen570@gmail.com>
2020-07-18 09:38:56 +0300

    Built  1st page components along w/ basic styling

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>Welcome to my blog</p>
+        </div>
+    )
+}
+
+export default Header
