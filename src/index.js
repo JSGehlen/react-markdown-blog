2cbd2047985176102825514931092abb9f8b4a24
JSGehlen <jeremygehlen570@gmail.com>
2020-07-19 22:36:38 +0300

    Added a scrolltotop component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  useLocation,
+  withRouter}from 'react-router-dom'
 import "typeface-livvic";
 import './styles/main.css'
 import Home from './pages/home'
@@ -9,13 +13,24 @@ import About from './pages/about'
 import Post from './pages/post'
 import NotFound from './pages/notfound'
 
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children
+}
+const ScrollToTop = withRouter(_ScrollToTop)
+
 ReactDOM.render(
   <Router>
     <div>
+      <ScrollToTop>
       <Route exact path="/" component={Home}/>
       <Route exact path="/about" component={About}/>
       <Route exact path="/404" component={NotFound}/>
       <Route exact path="/post/:id" render={props => <Post {...props}/>}/>
+      </ScrollToTop>
     </div>
   </Router>,
   document.getElementById('root')
